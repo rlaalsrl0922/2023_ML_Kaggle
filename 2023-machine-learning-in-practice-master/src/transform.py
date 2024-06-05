d77f6ad18324356024ef31f0600c69202be39725
rlaalsrl0922 <100950580+rlaalsrl0922@users.noreply.github.com>
2024-06-05 21:56:06 +0900

    Delete 2023-machine-learning-in-practice-master directory

--- 2023-machine-learning-in-practice-master/src/transform.py
+++
@@ -1,9 +0,0 @@
-from torchvision.transforms import Compose, Normalize, ToTensor
-
-
-def get_transform():
-    return Compose([ToTensor(), Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
-
-
-def get_test_transform():
-    return Compose([ToTensor(), Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
